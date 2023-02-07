e8118aae81e844dcbc964877e09ae7f89ce7076b
praveen <88870005+Praveen-ranganathan@users.noreply.github.com>
2023-02-07 14:20:48 +0530

    Add files via upload

---
+++ DAY1/src/loop/loop3.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class loop3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=100;i++) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
