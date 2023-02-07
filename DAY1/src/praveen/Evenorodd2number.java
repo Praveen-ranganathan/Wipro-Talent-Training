dc720ab75627ced025069561d1c2cfcb32ab46e3
praveen <88870005+Praveen-ranganathan@users.noreply.github.com>
2023-02-07 14:11:18 +0530

    Add files via upload

---
+++ DAY1/src/praveen/Evenorodd2number.java
@@ -0,0 +1,23 @@
+package praveen;
+
+import java.util.Scanner;
+
+public class Evenorodd2number {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter number1 :");
+		int inp1=sc.nextInt();
+		System.out.print("Enter number2 :");
+		int inp2=sc.nextInt();
+		int sum=inp1+inp2;
+		if(sum%2==0) {
+			System.out.println(" Sum isEVEN");
+		}
+		else {
+			System.out.println(" Sum is ODD");
+		}
+
+	}
+
+}
